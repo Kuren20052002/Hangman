27b7e5c412afb8738a1878c5477735e812fc4f3b
Nguyễn Hoàng Lâm <165152517+Kuren20052002@users.noreply.github.com>
2024-07-13 22:32:42 +0700

    Add man class to draw the hangerdud

---
+++ lib/hangman/man.rb
@@ -0,0 +1,16 @@
+# Drawing of hangman in every stage
+class Man
+  attr_reader :hangman_stages
+
+  def initialize
+    @hangman_stages = [
+      "  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
+      "  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=========",
+      "  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n=========",
+      "  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=========",
+      "  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n      |\n=========",
+      "  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n      |\n=========",
+      "  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n      |\n========="
+    ]
+  end
+end
