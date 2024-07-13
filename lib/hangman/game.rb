3a26fb5a0ac84a83a0df7e14592f9bf6c3623436
Nguyễn Hoàng Lâm <165152517+Kuren20052002@users.noreply.github.com>
2024-07-13 22:34:59 +0700

    Write function that handle input and output and etc

--- lib/hangman/game.rb
+++ lib/hangman/game.rb
@@ -8,4 +8,51 @@ class Game
     @guess = guess
     @answer = answer
   end
+
+  def pick_random_word
+    @answer = File.readlines("Words.txt").sample.split
+    @guess = Array.new(@answer.length)
+  end
+
+  def check_result
+    if guess == answer
+      puts "You won!"
+      1
+    elsif number_of_guesses_made == 8
+      puts "You lose"
+      -1
+    else
+      0
+    end
+  end
+
+  def reset
+    @number_of_guesses_made = 0
+    @guessed_words.push(@answer.join)
+    pick_random_word
+  end
+
+  def show_guessed_char
+    @guess.each do |letter|
+      if letter
+        puts "#{letter} "
+      else
+        puts "_ "
+      end
+    end
+  end
+
+  def fetch_guess
+    loop do
+      puts "Input guess"
+      input = gets.chomp
+      return input if input.length == 1 && input.match?(/\D/)
+    end
+  end
+
+  def start_game
+    while true
+
+    end
+  end
 end
